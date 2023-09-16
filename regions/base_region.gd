1377bc0792a9e102d5b1c3d1bfe38e372fb6c8dc
ruinthread-games <73795962+ruinthread-games@users.noreply.github.com>
2023-09-16 17:58:48 +0200

    Region highlighting (wip)

--- regions/base_region.gd
+++ regions/base_region.gd
@@ -1,5 +1,6 @@
 extends Spatial
 
+var base_colour
 
 var population;
 
@@ -12,3 +13,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func set_highlight_colour(colour,new_base_colour=false):
+	if new_base_colour:
+		base_colour = colour
+	var mesh_surface_mat = $MeshInstance.get_surface_material(0)
+	var new_mesh_surface_mat = mesh_surface_mat.duplicate()
+	var colour_to_set = colour
+	if colour == null:
+		colour_to_set = base_colour
+	new_mesh_surface_mat.albedo_color = colour_to_set
+	$MeshInstance.set_surface_material(0,new_mesh_surface_mat)
